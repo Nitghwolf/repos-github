00ba896dfa548857b2a594ca354ecc48f4f50577
Андрей Волков <nitghwolf13@gmail.com>
2022-06-23 16:48:02 +0300

    release input name

---
+++ src/component/AddName.js
@@ -0,0 +1,42 @@
+import React, {useState} from "react";
+import "./AddName.css";
+
+function useInputValue(defaultValue = ""){
+    const [value, setValue] = useState(defaultValue);
+
+    return{
+        bind: {
+            value,
+            onChange: event => setValue(event.target.value)
+        },
+        clear: () => setValue(""),
+        value: () => value
+    };
+}
+
+function AddName({onSend}){
+    const input = useInputValue("");
+
+    function submitHandler(event){
+        event.preventDefault();
+
+        if(input.value().trim()){
+            onSend(input.value());
+            input.clear();
+        }
+    }
+
+    return(
+        <div className="col-3 input-effect">
+            <form className="addNameForm" onSubmit={submitHandler}>
+                    <input {...input.bind} className="effect-20" type="text" placeholder="Введите имя"/>
+                    <span className="focus-border">
+                    <i></i>
+                </span>
+                <button type="submit" className="submit-feedback">Отправить ник</button>
+            </form>
+        </div>
+    );
+}
+
+export default AddName;
